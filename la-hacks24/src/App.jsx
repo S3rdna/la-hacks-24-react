85597346a9840ba9ff609dca7274684e74079662
S3rdna <aaguilar8079@gmail.com>
2024-04-20 18:55:07 -0700

    added react and server

--- la-hacks24/src/App.jsx
+++ la-hacks24/src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`submit button pressed!\nusername: ${username}\npassword: ${password}`)
+        //when submitted we need to send to backend
+        //
+        fetch('http://localhost:8888/api/login', { method: 'POST', body: JSON.stringify({ username, password }) })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label name="username">Username:</label>
+                    <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+
+
+                <div className="form-group">
+                    <label name="email">Email:</label>
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <input type='submit' value="submit" />
+            </form>
+
+        </>
+    )
 }
 
 export default App
